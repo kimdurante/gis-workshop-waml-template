d405bc070a665693d6531f234e6a6c91ea5e0a4a
Kim Durante <kimberlydurante@gmail.com>
2022-03-17 13:48:23 -0700

    Update arc_project.py

--- scripts/arc_project.py
+++ scripts/arc_project.py
@@ -4,13 +4,15 @@ import os
 
 #
 env.workspace = "W:\Africa_Marine_Atlas"
-#spatial reference
 sr = "W:\Africa_Marine_Atlas\ports.prj"
 
 for dirs, subdirs, files in os.walk(rootdir):
     for f in files:
         if f.endswith(".shp"):
             filePath = os.path.join(dirs, f)
-            print (filePath)
-            print (f, "...is being projected")
-            arcpy.DefineProjection_management(filePath, sr)
+            try:
+                spatial_ref = arcpy.Describe(filePath).spatialReference
+                print (spatial_ref.name, " is the projection for ", f)
+            else:
+                print (f, "...is being projected")
+                arcpy.DefineProjection_management(filePath, sr)
