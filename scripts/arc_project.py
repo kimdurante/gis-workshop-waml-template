e94fee7253f3b247a0ae94746cae0ff3e6ff3537
Kim Durante <kimberlydurante@gmail.com>
2022-04-14 13:09:28 -0700

    Update arc_project.py

--- scripts/arc_project.py
+++ scripts/arc_project.py
@@ -1,14 +1,16 @@
+#Reprojecting data (arc_project.py). Walk through a directory of shapefiles and GeoTIFFs, and reproject them to a specified projection.
 import arcpy
 from arcpy import env
 import os
 
-#
+#Path to the data directory
 env.workspace = "W:\Africa_Marine_Atlas"
+#Path to a projection file containing the desired projection metadata
 sr = "W:\Africa_Marine_Atlas\ports.prj"
 
 for dirs, subdirs, files in os.walk(rootdir):
     for f in files:
-        if f.endswith(".shp"):
+        if f.endswith(".shp") or f.endswith(".tif":
             filePath = os.path.join(dirs, f)
             try:
                 spatial_ref = arcpy.Describe(filePath).spatialReference
