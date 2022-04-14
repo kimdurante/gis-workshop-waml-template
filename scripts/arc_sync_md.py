ee94458c653444e5b0c9e1e707b922950d155ada
Kim Durante <kimberlydurante@gmail.com>
2022-04-14 12:56:57 -0700

    Update arc_sync_md.py

--- scripts/arc_sync_md.py
+++ scripts/arc_sync_md.py
@@ -1,14 +1,14 @@
+#Synchronize Metadata (arc_sync_md.py). Walk through a directory of shapefiles and GeoTIFFs, and update the metadata.
 import arcpy
 from arcpy import env
 import os
 
-#
+#Enter the path to the data directory
 env.workspace = "W:\Africa_Marine_Atlas"
-sr = "W:\Africa_Marine_Atlas\ports.prj"
 
-for dirs, subdirs, files in os.walk(rootdir):
+for dirs, subdirs, files in os.walk('.'):
     for f in files:
-        if f.endswith(".shp"):
+        if f.endswith(".shp") of f.endswith(".tif"):
             filePath = os.path.join(dirs, f)
             src_item_md = md.Metadata(filePath)
             print (src_item_md)
