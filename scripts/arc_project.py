f5d7374e2270707dad887a8c0d95e525d5cc7082
Kim Durante <kimberlydurante@gmail.com>
2022-04-21 14:30:17 -0700

    Update arc_project.py

--- scripts/arc_project.py
+++ scripts/arc_project.py
@@ -12,9 +12,5 @@ for dirs, subdirs, files in os.walk(env.workspace):
     for f in files:
         if f.endswith(".shp") or f.endswith(".tif"):
             filePath = os.path.join(dirs, f)
-            try:
-                spatial_ref = arcpy.Describe(filePath).spatialReference
-                print (spatial_ref.name, " is the projection for ", f)
-            else:
-                print (f, "...is being projected")
-                arcpy.DefineProjection_management(filePath, sr)
+            print (f, "...is being projected")
+            arcpy.DefineProjection_management(filePath, sr)
