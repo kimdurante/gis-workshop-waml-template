905ce0512a442adfd2580d643ad294c1c92861bd
Kim Durante <kimberlydurante@gmail.com>
2022-08-31 14:01:27 -0700

    Update rename.py

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,10 +1,12 @@
+## This script scans a directory of files and removes any characters in filenames which are not letters, numbers, or underscores. ##
+
 import os
 import re
 
 for dirName, subDirs, fileNames in os.walk('.'):
     for f in fileNames:
         oldFilename = os.path.join(dirName, f)
-        f  = re.sub('[^A-Za-z0-9._]+', '_', f)
+        f  = re.sub('[^A-Za-z0-9._]+', '', f)
         newFilename = os.path.join(dirName, f)
         print (oldFilename, newFilename)
         os.rename(oldFilename, newFilename)
