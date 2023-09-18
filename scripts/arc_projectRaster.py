3e32758ad2bfa2bb87416606a4e532ee0c90896c
Kim Durante <kimberlydurante@gmail.com>
2023-09-18 10:54:23 -0700

    Update arc_projectRaster.py

--- scripts/arc_projectRaster.py
+++ scripts/arc_projectRaster.py
@@ -3,20 +3,19 @@ import arcpy
 from arcpy import env
 import os
 
-#path to the data directory
-env.workspace = "W:\Africa_Marine_Atlas"
+#path to the data directory (Ex. "W:\Africa_Marine_Atlas")
+env.workspace = "PATH_TO_DIRECTORY"
 
 #add EPSG code
 outCS = arcpy.SpatialReference()
 outCS.factoryCode = 4326
 outCS.create()
 
-#path to the output directory
-outDir = "W:\Africa_Marine_Atlas\WGS84\"
+#path to the output directory (Ex. "W:\Africa_Marine_Atlas\WGS84\")
+outDir = "PATH_TO_DIRECTORY"
 
 for dirs, subdirs, files in os.walk(env.workspace):
     for f in files:
-        if f.startswith("MCE") and f.endswith(".3G_2020.tif"):
-            filePath = os.path.join(dirs, f)
-            print (f)
-            arcpy.management.ProjectRaster(filePath, outDir + f, outCS)
+        filePath = os.path.join(dirs, f)
+        print (f)
+        arcpy.management.ProjectRaster(filePath, outDir + f, outCS)
